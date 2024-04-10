b0466b00271b0f9162835896033f70f2b394cd31
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-04-10 03:09:27 +0200

    updated color scheme for linux

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,17 @@
-return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    "rebelot/kanagawa.nvim",
-    "catppuccin/nvim",
-    { "rose-pine/neovim", name = "rose-pine" },
-    {
-      "f-person/auto-dark-mode.nvim",
-      config = {
+local plugins = {
+  "ellisonleao/gruvbox.nvim",
+  "rebelot/kanagawa.nvim",
+  "catppuccin/nvim",
+  { "rose-pine/neovim", name = "rose-pine" },
+}
+
+-- Check if the system is macOS
+if vim.loop.os_uname().sysname == "Darwin" then
+  -- Add auto-dark-mode.nvim to the list of plugins with conditional configuration
+  table.insert(plugins, {
+    "f-person/auto-dark-mode.nvim",
+    config = function()
+      require("auto-dark-mode").setup({
         update_interval = 1000,
         set_dark_mode = function()
           vim.api.nvim_set_option("background", "dark")
@@ -16,7 +21,18 @@ return {
           vim.api.nvim_set_option("background", "light")
           vim.cmd("colorscheme rose-pine-dawn")
         end,
-      },
+      })
+      -- Activate auto-dark-mode
+      require("auto-dark-mode").init()
+    end,
+  })
+else
+  table.insert(plugins, {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine-moon",
     },
-  },
-}
+  })
+end
+
+return plugins
