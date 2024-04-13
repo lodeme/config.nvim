4096ac91a3137a8904a08868775db3a125ca73fd
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-04-13 12:16:40 +0200

    added file explorer with keymaps and disabled unnecessary plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,13 +2,17 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>b", require("osc52").copy_operator, { expr = true })
-vim.keymap.set("n", "<leader>bc", "<leader>c_", { remap = true })
-vim.keymap.set("v", "<leader>b", require("osc52").copy_visual)
-vim.keymap.set("n", "<leader>qq", "<cmd>wqa<cr>", { desc = "Save and Quit All" })
+vim.keymap.set("n", "<leader>qq", "<cmd>wa<cr><cmd>qa<cr>", { desc = "Save and Quit All" })
 vim.keymap.set("n", "<leader>qf", "<cmd>qa!<cr>", { desc = "Force Quit All" })
 
--- terminal keymaps
-local opts = { buffer = 0 }
-vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+-- file browser
+vim.keymap.set(
+  "n",
+  "<leader>fe",
+  "<cmd>lua require 'telescope'.extensions.file_browser.file_browser()<CR>",
+  { desc = "File Explorer" }
+)
+vim.keymap.set("n", "<space>fE", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", {desc = "File Explorer (buf cwd)"})
+
+-- Lazy
+vim.keymap.set("n", "<leader>X", "<cmd>LazyExtras<cr>", { desc = "LazyExtras"})
